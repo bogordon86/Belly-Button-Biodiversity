85d1df6738bed3aba97e32b85ebef48bc03d395b
Benjamin Gordon <bogordon86@gmail.com>
2018-03-01 12:37:27 -0500

    commit changes

--- static/index.js
+++ static/index.js
@@ -1,100 +1,207 @@
-//On Initial load, run with the sample set "BB_940"
-buildDropdown();
-buildMeta("BB_940");
-piePlot("BB_940");
-bubblePlot("BB_940");
+// --------------------------------------------------------------------------
+// Initializes names function tied to Flask app - populates selector
+// --------------------------------------------------------------------------
+d3.json("/names", function(error, response) {
+    // error trapping for failed call from Flask
+    if (error) return console.warn(error);
 
+    var $dropDown = document.getElementById("selDataset");
 
-//When a new sample is selected update the chart
-function optionChanged(sample){
-    buildMeta(sample);
-    piePlot(sample);
-    bubblePlot(sample);
-}
+    for (var i=0; i< response.length; i++){
+        var $optionChoice = document.createElement("option");
+        $optionChoice.innerHTML = response[i];
+        $optionChoice.setAttribute("value", response[i]);
+        $dropDown.appendChild($optionChoice);
+    };
+});
 
-// Build the dropdown by inputting sample names in HTML
-function buildDropdown() {
-    Plotly.d3.json('/names', function(error, names){
-        if (error) return console.warn(error);
-        var selDataset = document.getElementById("selDataset");
-        for (i = 0; i < names.length; i++) {
-                    sample = names[i]
-                    var sample_option = document.createElement("option");
-                    sample_option.text = sample;
-                    sample_option.value= sample;
-                    selDataset.appendChild(sample_option);
-        }
-    }
-)}
-// Function to build the metadata table in HTML template
-function buildMeta(sample) {
-    url = '/metadata/' + sample
-    Plotly.d3.json(url, function(error, data){
-       if (error) return console.warn(error);
+//Initializes the web page render
+function init(sample){
 
-       var age = document.getElementById("age");
-       var bbtype = document.getElementById("bbtype");
-       var gender = document.getElementById("gender");
-       var location = document.getElementById("location");
-       var sampleid = document.getElementById("sampleid");
+    getMetadata(sample);
 
-       age.innerHTML = data.AGE;
-       bbtype.innerHTML = data.BBTYPE;
-       gender.innerHTML = data.GENDER;
-       location.innerHTML = data.LOCATION;
-       sampleid.innerHTML = data.SAMPLEID;
-    }
-)}
-
-//Build the piePlot using a slice of ten
-function piePlot(sample){
-    url = '/samples/'+sample;
-    Plotly.d3.json(url, function(error, data){
+    //Gets data ready for the pie chart
+    d3.json("/samples/" + sample, function(error, sampleResponse){
+        // error trapping for failed call from Flask
         if (error) return console.warn(error);
 
-        var otu = data.map(record => record.otu_id);
-        var values =data.map(record => record.sample_value);
+        // parses repsonse data and take slice of first ten
+        // data pairs returned from Flask
+        resLabels = sampleResponse[0]["otu_ids"].slice(0,10);
+        resValues = sampleResponse[1]["sample_values"].slice(0,10);
 
-        var trace1 = {
-            labels: +otu.slice(0,10),
-            values:+values.slice(0,10),
-            type: 'pie'
-        }
-        var pieData = [trace1];
-        layout = {
-            height:400,
-            width:400,
-        }
-        var PIE = document.getElementById('piePlot');
-        Plotly.plot(PIE, pieData, layout);
-      }
-)}
-
-//Build the bubble chart for the index page
-function bubblePlot(sample){
-    url ='/samples/'+sample;
-    Plotly.d3.json(url, function(error, data){
-        if (error) return console.warn(error);
-        var otu = data.map(record => record.otu_id);
-        var values = data.map(record => record.sample_value);
-        var names = data.map(record => record.lowest_taxonomic_unit_found);
-
-        var trace1 = {
-            x: otu,
-            y: +values,
-            text: names,
-            mode: 'markers',
-            marker : {
-            size: values,
-            color: otu
-            }
+        for (var i=0; i<10; i++){
+            if (resLabels[i] == 0){
+                resLabels = resLabels.slice(0,i)
+            };
+            if (resValues[i] == 0){
+                resValues[i] = resValues.slice(0,i)
+            };
         };
-        var data = [trace1];
 
+        // ------------------------------------------------------------------
+        // get description matches of top ten bacteria and creates list
+        // ------------------------------------------------------------------
+        d3.json("/otu", function(error, response){
+        // error trapping for failed call from Flask
+        if (error) return console.warn(error);
+
+        var bacteriaNamesPie = [];
+        for (var i=0; i< resLabels.length; i++){
+            bacteriaNamesPie.push(response[resLabels[i]])
+        };
+
+        //  list of names for Bubble Chart
+        var bacteriaNamesBub = [];
+        for (var i =0; i<sampleResponse[0]["otu_ids"].length; i++){
+            bacteriaNamesBub.push(response[sampleResponse[0]["otu_ids"][i]])
+        };
+
+        // sets up data for pie chart
+        var data = [{
+                     values: resValues,
+                     labels: resLabels,
+                     hovertext: bacteriaNamesPie,
+                     hoverinfo: {bordercolor: 'black'},
+                     type: 'pie'
+        }];
+
+        // sets up layout for pie chart
         var layout = {
-            showlegend: false
+                      height: 500,
+                      title: "Top Sample Counts for " + sample
+                     };
+
+        // plots piechart
+        Plotly.newPlot('piePlot', data, layout);
+
+        //Sets up the Bubble Chart
+        var trace = {
+            x: sampleResponse[0]["otu_ids"],
+            y: sampleResponse[1]["sample_values"],
+            mode: 'markers',
+            marker: {
+                colorscale: 'Earth',
+                color: sampleResponse[0]["otu_ids"],
+                size: sampleResponse[1]["sample_values"]
+            },
+            text: bacteriaNamesBub,
+            type: "scatter"
         };
-        var BUBBLE = document.getElementById('bubblePlot')
-        Plotly.plot(BUBBLE, data, layout);
-    }
-)}
+
+        var bubbleData = [trace];
+
+        var bubbleLayout = {
+            title: 'Sample Values for ' + sample,
+            xaxis: {title: "OTU ID"},
+            hovermode: 'closest',
+            showlegend: false,
+            height: 600,
+            margin:
+                {
+                    top: 10,
+                    bottom: 10,
+                    right: 10,
+                    left: 10
+                }
+        };
+
+        // plots bubble chart
+        Plotly.newPlot('bubblePlot', bubbleData, bubbleLayout);
+      });
+    });
+
+    console.log("Initial Render Successful!");
+};
+
+// Function to update Pie Chart
+function updatePie(newValues, newLabels, newNames, sample_name){
+    Plotly.restyle("piePlot", "values", [newValues]);
+    Plotly.restyle("piePlot", "labels", [newLabels]);
+    Plotly.restyle("piePlot", "hovertext", [newNames]);
+    Plotly.relayout("piePlot", "title", "Top Sample Counts for " + sample_name);
+    console.log("Pie Chart Updated!");
+};
+// Function to update Bubble Chart
+function updateBub(values, labels, names, sample_name){
+    Plotly.restyle("bubblePlot", "x", [labels]);
+    Plotly.restyle("bubblePlot", "y", [values]);
+    Plotly.restyle("bubblePlot", "marker.size", [values]);
+    Plotly.restyle("bubblePlot", "text", [names]);
+    Plotly.relayout("bubblePlot", "title", "Sample Values for " + sample_name);
+    console.log("Bubble Chart Updated!");
+};
+
+// handle change in dropdown
+function optionChanged(chosenSample){
+  getMetadata(chosenSample);
+
+    // handle new get request for choice
+    d3.json("/samples/" + chosenSample, function(error, newResponse){
+      // error trapping for failed call from Flask
+      if (error) return console.warn(error);
+
+      var newResLabels = newResponse[0]["otu_ids"].slice(0,10);
+      var newResValues = newResponse[1]["sample_values"].slice(0,10);
+
+      for (var i=0; i<10; i++){
+        if (newResLabels[i] == 0){
+            newResLabels = resLabels.slice(0,i)
+        };
+        if (newResValues[i] == 0){
+            newResValues[i] = resValues.slice(0,i)
+        };
+      };
+
+      d3.json("/otu", function(error, otuResponse){
+        // error trapping for failed call from Flask
+        if (error) return console.warn(error);
+
+        var newBacteriaNames = [];
+
+        for (var i=0; i< newResLabels.length; i++){
+            newBacteriaNames.push(otuResponse[newResLabels[i]])
+        };
+        //  all bacteria names for bubble hover
+        var allBacteriaNames = [];
+        for (var i=0; i<newResponse[0]["otu_ids"].length; i++){
+            allBacteriaNames.push(otuResponse[newResponse[0]["otu_ids"][i]])
+        };
+
+        // new vars for updateBub function
+        var newValuesBub = newResponse[1]['sample_values'];
+        var newLabelsBub = newResponse[0]['otu_ids'];
+
+        // update plots
+        updatePie(newResValues, newResLabels, newBacteriaNames, chosenSample);
+        updateBub(newValuesBub, newLabelsBub, allBacteriaNames, chosenSample);
+      })
+
+  });
+};
+
+function getMetadata(selectedID){
+  d3.json("/metadata/" + selectedID, function(error, response){
+    // error trapping for failed call from Flask
+    if (error) return console.warn(error);
+
+    // get list of keys from response
+    var resKeys = Object.keys(response);
+
+    // Accesses the division in HTML
+    var $sampleMetadata = document.querySelector("#sample-metadata");
+
+    // clears panel
+    $sampleMetadata.innerHTML = null;
+
+    // iterates through keys and creates p element for each key
+    for (var i=0; i<resKeys.length; i++){
+        var $newDataLine = document.createElement('p');
+        $newDataLine.innerHTML = resKeys[i] + ": " + response[resKeys[i]];
+        $sampleMetadata.appendChild($newDataLine)
+    };
+  });
+};
+
+// initial render using first sample in dataset
+init("BB_940");
